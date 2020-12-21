b9222b984ebcd64110592bae5b30b586788de71a
DevD <devnesss@gmail.com>
2020-12-21 10:41:08 +0530

    Check Floating button direction change sorting con position changed

--- app/src/test/java/Reference.java
+++ app/src/test/java/Reference.java
@@ -4,8 +4,8 @@ public class Reference {
     *   -- Floating Camera Design
     *   --Check SpVariables Name
     *   --Once Close the floating then if open the app then floating button service is not showing
-    *   ++Check Floating button direction change
     *   --Home Screen design improve
+    * *   ++Check Floating button direction change
     *   --Setting Activity while scrolling the screen
     *   xx(remove) Sorting Filter on Home Screen
     *   ++ In Home Screen Option put List or grid
